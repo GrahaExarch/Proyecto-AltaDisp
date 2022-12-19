313ba9334a26db9a9445f07ae0955c6f4e54443d
Javier Ramos <javier.ramos1@mail.udp.cl>
2022-12-19 01:51:41 -0300

    fix al error del docker front y se agrega crucigrama

--- nuxt-frontend/pages/index.vue
+++ nuxt-frontend/pages/index.vue
@@ -6,8 +6,17 @@
     <v-col cols="4" lg="3">
       <ResumenCard type="UF" />
     </v-col>
-    <v-col cols="4" lg="3"> crucigrama </v-col>
     <v-col cols="4" lg="3"> temperatura </v-col>
+    <v-col cols="4" lg="3">
+      <v-card align="center">
+        <v-card-title class="headline"> Crucigrama Online: </v-card-title>
+        <v-btn class="mx-auto" color="primary"
+          ><a href="https://www.emol.com/servicios/juegos/crucigrama.aspx">
+            Ir
+          </a></v-btn
+        >
+      </v-card></v-col
+    >
   </v-row>
 </template>
 
@@ -16,3 +25,9 @@ export default {
   name: 'IndexPage',
 }
 </script>
+
+<style scoped>
+.v-application a {
+  color: white;
+}
+</style>
